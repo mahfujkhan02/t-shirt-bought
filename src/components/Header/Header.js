ff690e216381a25f1e6d2a0cc62ee1cf3496f25c
mahfujkhan02 <mahfujtanim450@gmail.com>
2024-03-06 12:19:30 +0600

    added routhing path

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <nav>
+            <Link to='/'>Home</Link>
+            <Link to='/about'>About</Link>
+            <Link to='/orders'>Orders</Link>
+        </nav>
+    );
+};
+
+export default Header;
