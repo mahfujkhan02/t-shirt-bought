31831a61b0f4eda38e5927a5f0ff19b9f01566a9
mahfujkhan02 <mahfujtanim450@gmail.com>
2024-03-08 02:32:22 +0600

    added condition in order summary

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,6 +1,24 @@
 import React from 'react';
 
 const Cart = ({cart , handleRemoveItem}) => {
+
+    let message;
+    if(cart.length === 0){
+        message = <p>please buy at least one item!!</p>
+    }
+    else if(cart.length === 1){
+        message = <div>
+            <h3>Amr jonou akta nao</h3>
+            <p>Tmr jonou nio akta</p>
+            <p><small>amr fr er jonou akta niou</small></p>
+        </div>
+
+    }
+    else{
+        message = <p>Thanks for buy!!!</p>
+    }
+
+
     return (
         <div>
             <h3>Order Summary</h3>
@@ -14,6 +32,9 @@ const Cart = ({cart , handleRemoveItem}) => {
                     {tshirt.name}
                     <button onClick={ () => handleRemoveItem(tshirt)}>X</button>
                 </p>) 
+            }
+            {
+                message
             }
         </div>
     );
